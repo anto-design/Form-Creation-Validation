4668c88fcadeb2e460e6b400cd4bc037212edf21
anto-design <spartz6122@gmail.com>
2025-01-21 18:05:17 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 document.addEventListener('DOMContentLoaded', function(event){
-
+})
 
   const form = document.getElementById('registration-form')
   const feedbackDiv = document.getElementById('form-feedback')
 
   form.addEventListener('submit' , (event)=>{
     event.preventDefault();
-  })
+
+    const usernameInput = document.getElementById('username')
+    const emailInput = document.getElementById('email')
+    const passwordInput = document.getElementById('password')
+    
+usernameInput.value.trim()
+ emailInput.value.trim()
+  passwordInput.value.trim()
 });
+
+
 
