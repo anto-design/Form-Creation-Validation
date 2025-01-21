85b7430ca8abac30020b1a1df436d634f6e022a0
anto-design <spartz6122@gmail.com>
2025-01-21 17:32:41 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 document.addEventListener('DOMContentLoaded', function(event){
-  
-})
+
+
+  const form = document.getElementById('registration-form')
+  const feedbackDiv = document.getElementById('form-feedback')
+
+  form.addEventListener('submit' , (event)=>{
+    event.preventDefault();
+  })
+});
 
